3858656f112951950dc353e28417d47727e76a19
Eric Xie <erictao6666@gmail.com>
2023-04-09 21:25:59 -0700

    I say...

---
+++ include/variables.h
@@ -0,0 +1,16 @@
+#ifndef VARIABLES_H
+#define VARIABLES_H
+#pragma once
+
+//Drive Ports
+inline constexpr int LEFTUP = 11;
+inline constexpr int LEFTMIDDLE = 3;
+inline constexpr int LEFTDOWN = 4;
+inline constexpr int RIGHTUP = 17;
+inline constexpr int RIGHTMIDDLE = 13;
+inline constexpr int RIGHTDOWN = 16;
+//Subsystem Ports
+inline constexpr int INTAKE = 19;
+inline constexpr int CATA = 1;
+
+#endif
